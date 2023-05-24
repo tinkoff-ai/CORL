212b91ba0624afe51581d2709915777a79a722d9
Denis Tarasov <dt6cgsg@gmail.com>
2023-05-24 14:48:14 +0300

    Rename CQL parameter and change adroit tuning parameters

--- algorithms/finetune/cql.py
+++ algorithms/finetune/cql.py
@@ -47,12 +47,13 @@ class TrainConfig:
     soft_target_update_rate: float = 5e-3  # Target network update rate
     bc_steps: int = int(0)  # Number of BC steps at start
     target_update_period: int = 1  # Frequency of target nets updates
+    cql_alpha: float = 10.0  # CQL offline regularization parameter
+    cql_alpha_online: float = 10.0  # CQL online regularization parameter
     cql_n_actions: int = 10  # Number of sampled actions
     cql_importance_sample: bool = True  # Use importance sampling
     cql_lagrange: bool = False  # Use Lagrange version of CQL
     cql_target_action_gap: float = -1.0  # Action gap
     cql_temp: float = 1.0  # CQL temperature
-    cql_min_q_weight: float = 10.0  # Minimal Q weight
     cql_max_target_backup: bool = False  # Use max target backup
     cql_clip_diff_min: float = -np.inf  # Q-function lower loss clipping
     cql_clip_diff_max: float = np.inf  # Q-function upper loss clipping
@@ -469,7 +470,7 @@ class ContinuousCQL:
         cql_lagrange: bool = False,
         cql_target_action_gap: float = -1.0,
         cql_temp: float = 1.0,
-        cql_min_q_weight: float = 5.0,
+        cql_alpha: float = 5.0,
         cql_max_target_backup: bool = False,
         cql_clip_diff_min: float = -np.inf,
         cql_clip_diff_max: float = np.inf,
@@ -492,7 +493,7 @@ class ContinuousCQL:
         self.cql_lagrange = cql_lagrange
         self.cql_target_action_gap = cql_target_action_gap
         self.cql_temp = cql_temp
-        self.cql_min_q_weight = cql_min_q_weight
+        self.cql_alpha = cql_alpha
         self.cql_max_target_backup = cql_max_target_backup
         self.cql_clip_diff_min = cql_clip_diff_min
         self.cql_clip_diff_max = cql_clip_diff_max
@@ -687,14 +688,14 @@ class ContinuousCQL:
                 torch.exp(self.log_alpha_prime()), min=0.0, max=1000000.0
             )
             cql_min_qf1_loss = (
-                alpha_prime  # noqa
-                * self.cql_min_q_weight  # noqa
-                * (cql_qf1_diff - self.cql_target_action_gap)  # noqa
+                    alpha_prime  # noqa
+                    * self.cql_alpha  # noqa
+                    * (cql_qf1_diff - self.cql_target_action_gap)  # noqa
             )
             cql_min_qf2_loss = (
-                alpha_prime  # noqa
-                * self.cql_min_q_weight  # noqa
-                * (cql_qf2_diff - self.cql_target_action_gap)  # noqa
+                    alpha_prime  # noqa
+                    * self.cql_alpha  # noqa
+                    * (cql_qf2_diff - self.cql_target_action_gap)  # noqa
             )
 
             self.alpha_prime_optimizer.zero_grad()
@@ -702,8 +703,8 @@ class ContinuousCQL:
             alpha_prime_loss.backward(retain_graph=True)
             self.alpha_prime_optimizer.step()
         else:
-            cql_min_qf1_loss = cql_qf1_diff * self.cql_min_q_weight
-            cql_min_qf2_loss = cql_qf2_diff * self.cql_min_q_weight
+            cql_min_qf1_loss = cql_qf1_diff * self.cql_alpha
+            cql_min_qf2_loss = cql_qf2_diff * self.cql_alpha
             alpha_prime_loss = observations.new_tensor(0.0)
             alpha_prime = observations.new_tensor(0.0)
 
@@ -925,7 +926,7 @@ def train(config: TrainConfig):
         "cql_lagrange": config.cql_lagrange,
         "cql_target_action_gap": config.cql_target_action_gap,
         "cql_temp": config.cql_temp,
-        "cql_min_q_weight": config.cql_min_q_weight,
+        "cql_alpha": config.cql_alpha,
         "cql_max_target_backup": config.cql_max_target_backup,
         "cql_clip_diff_min": config.cql_clip_diff_min,
         "cql_clip_diff_max": config.cql_clip_diff_max,
@@ -954,6 +955,7 @@ def train(config: TrainConfig):
     for t in range(int(config.offline_iterations) + int(config.online_iterations)):
         if t == config.offline_iterations:
             print("Online tuning")
+            trainer.cql_alpha = config.cql_alpha_online
         online_log = {}
         if t >= config.offline_iterations:
             episode_step += 1
